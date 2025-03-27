1d31932644cf7920903de08c34a907a324b8f272
juliusmarminge <jmarminge@gmail.com>
2025-03-27 14:31:42 +0100

    explain

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -11,11 +11,18 @@ import {
           staleTime: 30 * 1000,
         },
         dehydrate: {
-          // serializeData: superjson.serialize,
+          // include pending queries in dehydration
           shouldDehydrateQuery: (query) =>
             defaultShouldDehydrateQuery(query) ||
             query.state.status === 'pending',
-            shouldRedactErrors: () => false,
+          shouldRedactErrors: (error) => {
+            // We should not catch Next.js server errors
+            // as that's how Next.js detects dynamic pages
+            // so we cannot redact them.
+            // Next.js also automatically redacts errors for us
+            // with better digests.
+            return false
+          },
         },
         hydrate: {
           // deserializeData: superjson.deserialize,
